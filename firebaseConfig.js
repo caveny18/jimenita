ed38855e15f42ea5bcb542e93b6997bcbd0c507a
caveny18 <20231469@lamolina.edu.pe>
2025-08-19 14:16:44 -0500

    ll

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPqcugo5r3PmW38sMvL1OIQMj5kayUeSU",
+  authDomain: "agendaconsultorio-d6324.firebaseapp.com",
+  projectId: "agendaconsultorio-d6324",
+  storageBucket: "agendaconsultorio-d6324.firebasestorage.app",
+  messagingSenderId: "1064951207006",
+  appId: "1:1064951207006:web:32e4cd86e60ace92fe10cf"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
